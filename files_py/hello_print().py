763afef725adf492318330340715d78abd2ed7bf
Vitaly Tananykin <tfanfan@rambler.ru>
2024-01-28 10:56:26 +0300

    Add files via upload

---
+++ files_py/hello_print().py
@@ -0,0 +1,34 @@
+#a = '**'
+#print('My' + a + 'Name' + a + 'Is' + a +'James')
+#print('{:*^40}'.format('My**Name**Is**James'))
+#print("My","Name","Is","James", sep = "**")
+#city = 'Париж'
+#height = 300
+# ваш код
+#print("Эйфелева башня находится в "+ str(city) + "е, и её высота составляет " + str(height) + " метров")
+
+print("I\'m Monty Python.")
+print("\"I\'m\"")
+print("\" learning\"\" ")
+print("\"\"\"Python\"\"\"")
+print(float("{0:.0f}".format(3.141592653589793)))
+print(float("{0:.2f}".format(3.141592653589793)))
+print(float("{0:.4f}".format(3.141592653589793)))
+
+#number = 15
+#result = bin(number)
+#print(result)
+# a = 15
+# b = 16
+# c = a / b
+# print('+15')
+# print(bin(15)[2:])
+# print(f'{15:x}')
+# #printfloat((hex(0.9375)[2:]))
+# print(c)
+# N = input()
+# if N in range(12):
+#     yeartme = check(winter_list, spring_list, summer_list, Autumn_list)
+#     print(yeartme)
+# else:
+#     print('NO')
